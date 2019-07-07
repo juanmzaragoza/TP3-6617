b89befd3ef2b151ab1d1a419e474c9443fde93da
JMZ <jzaragoza@fi.uba.ar>
2019-07-07 13:21:05 -0300

    Se agrega interfaz del controlador de rotacion

---
+++ sources_1/new/ctrl_rotation.vhd
@@ -0,0 +1,54 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/07/2019 01:10:35 PM
+-- Design Name: 
+-- Module Name: ctrl_rotation - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity ctrl_rotation is
+    port(
+        -- inputs
+		clk, rst:                 in std_logic;
+		char_data:                in std_logic_vector(7 downto 0);
+		-- outputs
+		rotation_enable:          out std_logic;
+		degrees:                  out std_logic
+	);
+end ctrl_rotation;
+
+-- IDEA: recibo un char en char_data y lo voy guardando en memoria
+-- Si se forma ROT C A o ROT C H se generara rotation_enable=1 y la cantidad de grados se modifica de manera continua cada un tiempo visualizable (1seg)
+-- Si se forma ROT A angulo se genera rotation_enable=1 y la cantidad de grados de manera fija
+-- Mientras se ingrese algo distinto se genera rotation_enable=0 y deshabilita la rotacion
+architecture Behavioral of ctrl_rotation is
+
+begin
+
+
+end Behavioral;
