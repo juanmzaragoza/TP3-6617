679c20eda26aa93ba669ed0a79d1d09e16aa93e9
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2019-07-18 14:55:32 -0300

    Se agregan los rangos de ASCII

--- sources_1/new/ctrl_rotation.vhd
+++ sources_1/new/ctrl_rotation.vhd
@@ -24,7 +24,7 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
@@ -52,6 +52,11 @@ end ctrl_rotation;
 -- Si se forma ROT A angulo se genera rotation_enable=1 y la cantidad de grados de manera fija
 -- Mientras se ingrese algo distinto se genera rotation_enable=0 y deshabilita la rotacion
 architecture Behavioral of ctrl_rotation is
+    
+    function word_to_int (word : in std_logic_vector(7 downto 0))
+        begin
+            return to_integer(unsigned(word));
+        end;
 
     type buffer_type is array (0 to BUFFER_CHARS_SIZE-1) of std_logic_vector(CHAR_SIZE-1 downto 0);
     
@@ -82,16 +87,17 @@ begin
             if rotation_enable_aux = '1' then -- para que el rotation enable sea solo un pulso
                 rotation_enable_aux <= '0';
             else
-                if buffer_chars(BUFFER_CHARS_SIZE-1) and buffer_chars(BUFFER_CHARS_SIZE-2) and buffer_chars(BUFFER_CHARS_SIZE-3) then -- si es un numero => ROT A 000
+                -- 48=>[0], 57=>[9], 32=>[espacio], 67=>[C], 99=>[c], 84=>[T], 116=>[t], 79=>[O], 111=>[o], 82=>[R], 114=>[r], 65=>[A], 97=>[a], 72=>[H], 104=>[h],
+                if (word_to_int(buffer_chars(BUFFER_CHARS_SIZE-1)) >= 48 and word_to_int(buffer_chars(BUFFER_CHARS_SIZE-1)) <= 57) and (word_to_int(buffer_chars(BUFFER_CHARS_SIZE-2)) >= 48 and word_to_int(buffer_chars(BUFFER_CHARS_SIZE-2)) <= 57) and (word_to_int(buffer_chars(BUFFER_CHARS_SIZE-3)) >= 48 and word_to_int(buffer_chars(BUFFER_CHARS_SIZE-3)) <= 51) then -- si es un numero => ROT A [0-3][0-9][0-9]
                     
-                    if buffer_chars(BUFFER_CHARS_SIZE-4) and buffer_chars(BUFFER_CHARS_SIZE-5) and buffer_chars(BUFFER_CHARS_SIZE-6) and buffer_chars(BUFFER_CHARS_SIZE-7) and buffer_chars(BUFFER_CHARS_SIZE-8) and buffer_chars(BUFFER_CHARS_SIZE-9) then -- espero un espacio, una A, un espacio y TOR => ROT C 000
+                    if buffer_chars(BUFFER_CHARS_SIZE-4) = 32 and (buffer_chars(BUFFER_CHARS_SIZE-5) = 65 or buffer_chars(BUFFER_CHARS_SIZE-5) = 97) and buffer_chars(BUFFER_CHARS_SIZE-6) = 32 and (buffer_chars(BUFFER_CHARS_SIZE-7) = 84 or buffer_chars(BUFFER_CHARS_SIZE-7) = 116) and (buffer_chars(BUFFER_CHARS_SIZE-8) = 79 or buffer_chars(BUFFER_CHARS_SIZE-8) = 111) and (buffer_chars(BUFFER_CHARS_SIZE-9) = 82 or buffer_chars(BUFFER_CHARS_SIZE-9) = 114) then -- espero un espacio, una A, un espacio y TOR => ROT A 000
                         rotation_enable_aux <= '1';
                         degrees <= 100; --TODO: transformar los grados del buffer
                     end if;
                     
-                elsif buffer_chars(BUFFER_CHARS_SIZE-1) then -- tiene que ser una H o A
+                elsif (buffer_chars(BUFFER_CHARS_SIZE-1) = 65 or buffer_chars(BUFFER_CHARS_SIZE-1) = 97) or (buffer_chars(BUFFER_CHARS_SIZE-1) = 72 or buffer_chars(BUFFER_CHARS_SIZE-1) = 104) then -- tiene que ser una H o A
                 
-                    if buffer_chars(BUFFER_CHARS_SIZE-2) and buffer_chars(BUFFER_CHARS_SIZE-3) and buffer_chars(BUFFER_CHARS_SIZE-4) and buffer_chars(BUFFER_CHARS_SIZE-5) and buffer_chars(BUFFER_CHARS_SIZE-6) and buffer_chars(BUFFER_CHARS_SIZE-7) then -- espero un espacio, una C, un espacio y TOR => ROT C 000
+                    if buffer_chars(BUFFER_CHARS_SIZE-2) = 32 and (buffer_chars(BUFFER_CHARS_SIZE-3) = 67 or buffer_chars(BUFFER_CHARS_SIZE-3) = 99)  and buffer_chars(BUFFER_CHARS_SIZE-4) = 32 and (buffer_chars(BUFFER_CHARS_SIZE-5) = 84 or buffer_chars(BUFFER_CHARS_SIZE-5) = 116) and (buffer_chars(BUFFER_CHARS_SIZE-6) = 79 or buffer_chars(BUFFER_CHARS_SIZE-6) = 111) and (buffer_chars(BUFFER_CHARS_SIZE-7) = 82 or buffer_chars(BUFFER_CHARS_SIZE-7) = 114) then -- espero un espacio, una C, un espacio y TOR => ROT C 000
                         rotation_enable_aux <= '1';
                         degrees <= acc_degrees + 1;
                         acc_degrees <= acc_degrees + 1;
